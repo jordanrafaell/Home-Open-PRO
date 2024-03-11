a4fd5466e2c2c49894a48514180e9cbe0d09c31f
jordanrafaell <jordanrafaelflorencio@gmail.com>
2024-03-10 23:08:18 -0300

    testemunhas

---
+++ scripts/scripts.js
@@ -0,0 +1,17 @@
+let btn_play = document.querySelector('#btn_play');
+let video_play = document.querySelector('.videoPlayer');
+
+btn_play.addEventListener('click', function () {
+  video_play.classList.toggle('active_video');
+  btn_play.style.display = 'none';
+});
+
+document.addEventListener('click', function (event) {
+  // Verifica se o clique foi fora do vídeo e do botão de play
+  if (!video_play.contains(event.target) && event.target !== btn_play) {
+    video_play.classList.remove('active_video');
+    btn_play.style.display = 'block';
+  }
+});
+
+console.log(teste);
